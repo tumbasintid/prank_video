32a588bfbf9128a17c272191d763cd6ef9311e38
tumbasintid <aksaralokaa1@gmail.com>
2026-09-13 03:50:40 +0700

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// Ini adalah file script.js
-console.log("JavaScript berhasil terhubung dengan halaman video!");
-
-// Contoh interaksi sederhana saat video diklik
-const video = document.getElementById('mainVideo');
-video.addEventListener('click', function() {
-    console.log("Video sedang diklik/diputar oleh penonton.");
-});
-
